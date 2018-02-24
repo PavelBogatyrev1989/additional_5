69655ec4b78e954f89e9e3def44f02ca5c9806e4
Pavel Bogatyrev <bogatyrevpavel1989@gmail.com>
2018-02-25 02:20:40 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,62 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-
-
   if (str.length%2>0){
     return false;
   }
-
-
   openBrackets = '';
   closeBrackets = '';
   status = true;
-
   for (var i = 0; i < bracketsConfig.length; i++) {
-
-
     openBrackets = openBrackets + bracketsConfig[i][0].toString();
     closeBrackets = closeBrackets + bracketsConfig[i][1].toString();
   }
-
-
   stack = [];
-
-
-
- 
   for (var i = 0; i <= str.length; i++) {
 
-    if (openBrackets.indexOf(str[i]) >= 0) {
+    if (openBrackets.indexOf(str[i]) >= 0 ) {
       stack.push(str[i]);
-     console.log("++" );
     } 
     else
       if (closeBrackets.indexOf(str[i]) >= 0) {
         if (stack.length == 0) {       
-         console.log("ERROR");
           status = false;
           break;
         } else if (openBrackets.indexOf(stack[stack.length - 1]) != closeBrackets.indexOf(str[i])) {
-        // console.log( stack + " stack "+openBrackets[openBrackets.indexOf(stack[stack.length - 1])] + "!=" + closeBrackets [closeBrackets.indexOf(str[i])]);
           status = false;
           break;
 
         } else {
-
           stack.pop()
-         console.log("--");
         }
       }
 
 
 
   }
-
-
-
-
-
-
- console.log("status " + stack);
   return status;
-
 }
